f4afc0540eb412ff6f56f36a7632ef4221e409f3
Robin <hongxing525@126.com>
2025-10-31 22:46:57 +0800

    custom name

--- extensions/vscode/custom.sh
+++ extensions/vscode/custom.sh
@@ -15,24 +15,25 @@ fi
 
 # custom plugin
 # find ../web/ -type f -name "*.ts" -o -name "*.tsx" | xargs grep -E "[ '\"]Dify[ '\"]"| awk -F ':' '{print $1}' | xargs -I@ bash -c "${xsed} -i.bak 's#Dify#LLMAI#g' @"
-$xsed 's#"name": "continue",#"name": "aicoder",#g' ../../extensions/vscode/package.json
-$xsed 's#Continue Dev#BlockAI Dev#g' ../../extensions/vscode/package.json
-$xsed 's#"homepage": "https://continue.dev"#"homepage": "https://aicoder.dev"#g' ../../extensions/vscode/package.json
-$xsed 's#https://hub.continue.dev/#https://hub.continue.dev/#g' ../../extensions/vscode/package.json
-$xsed 's#https://github.com/continuedev/continue#https://github.com/blockai/aicoder#g' ../../extensions/vscode/package.json
+# $xsed 's#"name": "continue",#"name": "aicoder",#g' ../../extensions/vscode/package.json
+# $xsed 's#Continue Dev#BlockAI Dev#g' ../../extensions/vscode/package.json
+# $xsed 's#"homepage": "https://continue.dev"#"homepage": "https://aicoder.dev"#g' ../../extensions/vscode/package.json
+# $xsed 's#https://hub.continue.dev/#https://hub.continue.dev/#g' ../../extensions/vscode/package.json
+# $xsed 's#https://github.com/continuedev/continue#https://github.com/blockai/aicoder#g' ../../extensions/vscode/package.json
 # $xsed "s#continue#aicoder#g" ../../extensions/vscode/package.json
 # $xsed "s#continue#aicoder#g" ../../extensions/vscode/package.json
 $xsed 's#"displayName":.*#"displayName": "分布式编码助手",#g' ../../extensions/vscode/package.json
-$xsed 's#"publisher":.*#"publisher": "BlockAI",#g' ../../extensions/vscode/package.json
-$xsed 's#"title": "Continue",#"title": "分布式编码助手",#g' ../../extensions/vscode/package.json
-$xsed 's#"title": "Continue Console",#"title": "编码助手控制台",#g' ../../extensions/vscode/package.json
-$xsed 's#"Enable Continue#"Enable AiCoder#g' ../../extensions/vscode/package.json
-$xsed 's#"Pause Continue#"Pause AiCoder#g' ../../extensions/vscode/package.json
-$xsed 's#"name": "Continue#"name": "AiCoder#g' ../../extensions/vscode/package.json
+$xsed 's#"description":.*#"description": "编码智能体，助效能提升",#g' ../../extensions/vscode/package.json
+# $xsed 's#"publisher":.*#"publisher": "BlockAI",#g' ../../extensions/vscode/package.json
+# $xsed 's#"title": "Continue",#"title": "分布式编码助手",#g' ../../extensions/vscode/package.json
+# $xsed 's#"title": "Continue Console",#"title": "编码助手控制台",#g' ../../extensions/vscode/package.json
+# $xsed 's#"Enable Continue#"Enable AiCoder#g' ../../extensions/vscode/package.json
+# $xsed 's#"Pause Continue#"Pause AiCoder#g' ../../extensions/vscode/package.json
+# $xsed 's#"name": "Continue#"name": "AiCoder#g' ../../extensions/vscode/package.json
 
 
-# $xsed "s#logo-monochrome-white.svg#logo-monochrome-white-llmapp.svg#g" ../web/app/components/base/logo/logo-site.tsx
-find ../../ -type f -name "*.ts" -o -name "*.tsx" -o -name "*.js" -o -name "*.mdx" | grep -v node_modules | xargs $xsed 's#https://hub.continue.dev/#https://hub.aicoder.dev/#g'
+# # $xsed "s#logo-monochrome-white.svg#logo-monochrome-white-llmapp.svg#g" ../web/app/components/base/logo/logo-site.tsx
+# find ../../ -type f -name "*.ts" -o -name "*.tsx" -o -name "*.js" -o -name "*.mdx" | grep -v node_modules | xargs $xsed 's#https://hub.continue.dev/#https://hub.aicoder.dev/#g'
 # find ../web -type f -name "*.ts" -o -name "*.tsx" -o -name "*.js" | grep -v node_modules | xargs $xsed "s#logo-monochrome-white.svg#logo-monochrome-white-llmapp.svg#g"
 # find ../web -type f -name "*.ts" -o -name "*.tsx" | xargs grep -E "[ '>\"]Dify[ '<\"\$\`]" | grep -vE "default as Dify |Dify.json|embedded-chatbot/index.tsx"
 # find ../web -type f -name "*.ts" -o -name "*.tsx" | xargs grep -E "[ '>\"]Dify[ '<\"\$\`]" | grep -vE "default as Dify |Dify.json|embedded-chatbot/index.tsx" | awk -F ':' '{print $1}' | xargs -I@ sh -c "sed -i.bak 's#Dify#LLMAI#g' '@'"
