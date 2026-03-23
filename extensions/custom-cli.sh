39da748f387fc69dae4ddda4e5ad96b0699065a1
Robin <luoweb@users.noreply.github.com>
2026-03-23 23:08:09 +0800

    custom cli

---
+++ extensions/custom-cli.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+baseDir=$(cd `dirname $0`;pwd)
+cd $baseDir
+execStartTime=`date +%Y%m%d-%H:%M:%S`
+
+echo "${execStartTime} Exe Dir: $baseDir"
+xsed='sed -i'
+system=`uname`
+if [ "$system" == "Darwin" ]; then
+  echo "This is macOS"
+  xsed="sed -i .bak"
+else
+  echo "This is Linux"
+  xsed='sed -i'
+fi
+
+echo "########## custom replace begin ########## "
+
+echo "readme custom"
+cp ${baseDir}/../extensions/vscode/README.zh.md ${baseDir}/../extensions/vscode/README.md
+
+echo "ui custom"
+
+# $xsed 's#"name": "@continuedev/cli"#"name": "@aicoder/cli"#g' ${baseDir}/../extensions/cli/package.json
+$xsed 's#"description": "Continue CLI"#"description": "AICoder CLI"#g' ${baseDir}/../extensions/cli/package.json
